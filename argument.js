f0d9c5c768eaf606a516efae7f2559d4b3ad9236
Idba1 <moniraislamself@gmail.com>
2024-02-03 04:28:02 +0600

    argument.js

---
+++ argument.js
@@ -0,0 +1,21 @@
+function doubleIt (number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+
+console.log('I will call the function')
+doubleIt(15);
+
+
+
+
+
+/* Difference */
+function difference (num1, num2){
+    const diff = num1 - num2;
+    console.log(num1, num2, 'difference is', diff);
+}
+const fatherAge = 50;
+const myAge = 22;
+
+difference(fatherAge, myAge);
