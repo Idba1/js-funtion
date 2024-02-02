816af00b93c13f968bd06a5e02086e7384cab2e6
Idba1 <moniraislamself@gmail.com>
2024-02-03 04:18:03 +0600

    parameter.js

---
+++ parameter.js
@@ -0,0 +1,17 @@
+function addNumber (a, b){
+    const sum = a+b;
+    console.log(sum);
+}
+addNumber(3,8);
+
+
+
+
+
+
+function square(number) {
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('square of the given number is: ', borgo);
+}
+square(5,5);
