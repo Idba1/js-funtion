a5ca37cb89d9ac5e5762ed4e91c62cc989168551
Idba1 <moniraislamself@gmail.com>
2024-02-03 04:02:02 +0600

    funtion.js

---
+++ funtion.js
@@ -0,0 +1,14 @@
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the Fan');
+}
+fanOffKor();
+
+
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso and goso and fena felo');
+}
+brushTeeth();
